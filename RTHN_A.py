666ce631d1ca153fa4b2c1fc32487ae1049cb83d
1037562613@qq.com <1037562613@qq.com>
2020-04-16 20:53:41 +0800

    aaa

--- RTHN_A.py
+++ RTHN_A.py
@@ -37,8 +37,7 @@ tf.app.flags.DEFINE_float('lr_main', 0.001, 'learning rate')
 tf.app.flags.DEFINE_float('keep_prob1', 0.5, 'word embedding training dropout keep prob')
 tf.app.flags.DEFINE_float('keep_prob2', 1.0, 'softmax layer dropout keep prob')
 tf.app.flags.DEFINE_float('l2_reg', 1e-5, 'l2 regularization')
-tf.app.flags.DEFINE_integer('run_times', 1, 'run times of this model')
-#tf.app.flags.DEFINE_integer('run_times', 10, 'run times of this model')
+tf.app.flags.DEFINE_integer('run_times', 10, 'run times of this model')
 tf.app.flags.DEFINE_integer('num_heads', 5, 'the num heads of attention')
 tf.app.flags.DEFINE_integer('n_layers', 2, 'the layers of transformer beside main')
 
@@ -47,11 +46,13 @@ tf.app.flags.DEFINE_integer('n_layers', 2, 'the layers of transformer beside mai
 def build_model(x, sen_len, doc_len, word_dis, word_embedding, pos_embedding, keep_prob1, keep_prob2, RNN=func.biLSTM):
     x = tf.nn.embedding_lookup(word_embedding, x)#选取wordembedding中x对应的元素
     inputs = tf.reshape(x, [-1, FLAGS.max_sen_len, FLAGS.embedding_dim])
+    # print("word_dis:{}".format(word_dis))
     word_dis = tf.nn.embedding_lookup(pos_embedding, word_dis)
     sh2 = 2 * FLAGS.n_hidden
 
     inputs = tf.nn.dropout(inputs, keep_prob=keep_prob1)
     sen_len = tf.reshape(sen_len, [-1])
+    # print("sen_len:{}".format(sen_len))
     with tf.name_scope('word_encode'):
         wordEncode = RNN(inputs, sen_len, n_hidden=FLAGS.n_hidden, scope=FLAGS.scope + 'word_layer')
     wordEncode = tf.reshape(wordEncode, [-1, FLAGS.max_sen_len, sh2])
@@ -62,7 +63,11 @@ def build_model(x, sen_len, doc_len, word_dis, word_embedding, pos_embedding, ke
         w2 = func.get_weight_varible('word_att_w2', [sh2, 1])
         senEncode = func.att_var(wordEncode, sen_len, w1, b1, w2)
     senEncode = tf.reshape(senEncode, [-1, FLAGS.max_doc_len, sh2])
+
+    # print("word_dis:{}".format(word_dis))
     word_dis = tf.reshape(word_dis[:, :, 0, :], [-1, FLAGS.max_doc_len, FLAGS.embedding_dim_pos])
+    # print("word_dis:{}".format(word_dis))
+    # print("senEncode:{}".format(senEncode))
     senEncode_dis = tf.concat([senEncode, word_dis], axis=2)  # 距离拼在子句上
 
     n_feature = 2 * FLAGS.n_hidden + FLAGS.embedding_dim_pos
@@ -131,16 +136,18 @@ def run():
     tf.reset_default_graph()
     localtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     print("***********localtime: ", localtime)
-    #func.load_data()：return x, y, sen_len, doc_len, relative_pos, embedding, embedding_pos
-    x_data, y_data, sen_len_data, doc_len_data, word_distance, word_embedding, pos_embedding = func.load_data()
+    #func.load_data()： return x, y, sen_len, doc_len, relative_pos,relative_pos_a,  embedding, embedding_pos
+    #需要将word_distance改为自己计算的结果
+    x_data, y_data, sen_len_data, doc_len_data, word_distance, word_distance_a, word_embedding, pos_embedding = func.load_data()
 
-    # print("x_data.shape:{}\n".format(x_data.shape))
-    # print("y_data.shape:{}\n".format(y_data.shape))
-    # print("sen_len_data.shape:{}\n".format(sen_len_data.shape))
-    # print("doc_len_data.shape:{}\n".format(doc_len_data.shape))
-    # print("word_distance.shape:{}\n".format(word_distance.shape))
-    # print("word_embedding.shape:{}\n".format(word_embedding.shape))
-    # print("pos_embedding.shape:{}\n".format( pos_embedding.shape))
+    print("x_data.shape:{}\n".format(x_data.shape))
+    print("y_data.shape:{}\n".format(y_data.shape))
+    print("sen_len_data.shape:{}\n".format(sen_len_data.shape))
+    print("doc_len_data.shape:{}\n".format(doc_len_data.shape))
+    print("word_distance.shape:{}\n".format(word_distance.shape))
+    print("word_embedding.shape:{}\n".format(word_embedding.shape))
+    print("pos_embedding.shape:{}\n".format(pos_embedding.shape))
+    print("pos_embedding:{}\n".format(pos_embedding[1]))
 
     word_embedding = tf.constant(word_embedding, dtype=tf.float32, name='word_embedding')
     pos_embedding = tf.constant(pos_embedding, dtype=tf.float32, name='pos_embedding')
@@ -153,12 +160,13 @@ def run():
     y = tf.placeholder(tf.float32, [None, FLAGS.max_doc_len, FLAGS.n_class], name = "y")
     sen_len = tf.placeholder(tf.int32, [None, FLAGS.max_doc_len],name = "sen_len")
     doc_len = tf.placeholder(tf.int32, [None],name = "doc_len")
-    word_dis = tf.placeholder(tf.int32, [None, FLAGS.max_doc_len, FLAGS.max_sen_len], name = "word_dis")
+    word_dis_a = tf.placeholder(tf.int32, [None, FLAGS.max_doc_len, FLAGS.max_sen_len], name = "word_dis_a")
     keep_prob1 = tf.placeholder(tf.float32, name = "keep_prob1")
     keep_prob2 = tf.placeholder(tf.float32, name = "keep_prob2")
-    placeholders = [x, y, sen_len, doc_len, word_dis, keep_prob1, keep_prob2]
+    placeholders = [x, y, sen_len, doc_len, word_dis_a, keep_prob1, keep_prob2]
 
-    pred, reg, pred_assist_list, reg_assist_list = build_model(x, sen_len, doc_len, word_dis, word_embedding, pos_embedding, keep_prob1, keep_prob2)
+    pred, reg, pred_assist_list, reg_assist_list = build_model(x, sen_len, doc_len, word_dis_a, word_embedding, pos_embedding, keep_prob1, keep_prob2)
+    print(pred)
 
     with tf.name_scope('loss'):
         valid_num = tf.cast(tf.reduce_sum(doc_len), dtype=tf.float32)
@@ -180,6 +188,8 @@ def run():
 
     true_y_op = tf.argmax(y, 2,name = "true_y_op")
     pred_y_op = tf.argmax(pred, 2,  name = "pred_y_op")
+    print("true_y_op:{}".format(true_y_op))
+    print("pred_y_op:{}".format(pred_y_op))
     pred_y_assist_op_list = []
     for i in range(FLAGS.n_layers - 1):
         pred_y_assist_op = tf.argmax(pred_assist_list[i], 2)
@@ -196,21 +206,22 @@ def run():
 
     saver = tf.train.Saver(max_to_keep=4)
 
+    tf_config = tf.ConfigProto(device_count={'GPU': 0})
     with tf.Session(config=tf_config) as sess:
-        kf, fold, SID = KFold(n_splits=2), 1, 0 #十折交叉验证
+        kf, fold, SID = KFold(n_splits=10), 1, 0 #十折交叉验证
         Id = []
         p_list, r_list, f1_list = [], [], []
         for train, test in kf.split(x_data):
             tr_x, tr_y, tr_sen_len, tr_doc_len, tr_word_dis = map(lambda x: x[train],
-                [x_data, y_data, sen_len_data, doc_len_data, word_distance])
+                [x_data, y_data, sen_len_data, doc_len_data, word_distance_a])
             te_x, te_y, te_sen_len, te_doc_len, te_word_dis = map(lambda x: x[test],
-                [x_data, y_data, sen_len_data, doc_len_data, word_distance])
+                [x_data, y_data, sen_len_data, doc_len_data, word_distance_a])
             precision_list, recall_list, FF1_list = [], [], []
             pre_list, true_list, pre_list_prob = [], [], []
 
             sess.run(tf.global_variables_initializer())
             print('############# fold {} ###############'.format(fold))
-            fold += 1
+            # fold += 1
             max_f1 = 0.0
             print('train docs: {}    test docs: {}'.format(len(tr_y), len(te_y)))
 
@@ -243,58 +254,52 @@ def run():
                     acc, p, r, f1 = func.acc_prf(pred_y, true_y, doc_len_batch)
                     if step % 5 == 0:
                         print('epoch {}: step {}: loss {:.4f} acc {:.4f}'.format(epoch + 1, step, loss, acc))
-                        print("begin save!")
-                        saver.save(sess, "./run_2/model.ckpt",global_step=step)
                     step = step + 1
+                print("begin save!")
+                saver.save(sess, "./run_final_a/model.ckpt", global_step=step)
 
-        #         '''*********Test********'''
-        #         test = [te_x, te_y, te_sen_len, te_doc_len, te_word_dis, 1., 1.]
-        #         loss, pred_y, true_y, pred_prob = sess.run(
-        #             [loss_op, pred_y_op, true_y_op, pred], feed_dict=dict(zip(placeholders, test)))
-        #
-        #         end_time = time.time()
-        #
-        #         true_list.append(true_y)
-        #         pre_list.append(pred_y)
-        #         pre_list_prob.append(pred_prob)
-        #
-        #         #计算精确率准确率召回率和F值
-        #         acc, p, r, f1 = func.acc_prf(pred_y, true_y, te_doc_len)
-        #         precision_list.append(p)
-        #         recall_list.append(r)
-        #         FF1_list.append(f1)
-        #         if f1 > max_f1:
-        #             max_acc, max_p, max_r, max_f1 = acc, p, r, f1
-        #         print('\ntest: epoch {}: loss {:.4f} acc {:.4f}\np: {:.4f} r: {:.4f} f1: {:.4f} max_f1 {:.4f}\n'.format(
-        #             epoch + 1, loss, acc, p, r, f1, max_f1))
-        #
-        #     Id.append(len(te_x))
-        #     SID = np.sum(Id) - len(te_x)
-        #     _, maxIndex = func.maxS(FF1_list)
-        #     print("maxIndex:", maxIndex)
-        #     print('Optimization Finished!\n')
-        #     pred_prob = pre_list_prob[maxIndex]
-        #
-        #     for i in range(pred_y.shape[0]):
-        #         for j in range(te_doc_len[i]):
-        #             prob_list_pr.append(pred_prob[i][j][1])
-        #             y_label.append(true_y[i][j])
-        #
-        #     p_list.append(max_p)
-        #     r_list.append(max_r)
-        #     f1_list.append(max_f1)
-        # print("running time: ", str((end_time - start_time) / 60.))
-        # print_training_info()
-        # p, r, f1 = map(lambda x: np.array(x).mean(), [p_list, r_list, f1_list])
-        # print("f1_score in 10 fold: {}\naverage : {} {} {}\n".format(np.array(f1_list).reshape(-1, 1), round(p, 4), round(r, 4), round(f1, 4)))
+                '''*********Test********'''
+                test = [te_x, te_y, te_sen_len, te_doc_len, te_word_dis, 1., 1.]
+                loss, pred_y, true_y, pred_prob = sess.run(
+                    [loss_op, pred_y_op, true_y_op, pred], feed_dict=dict(zip(placeholders, test)))
+
+                end_time = time.time()
+
+                true_list.append(true_y)
+                pre_list.append(pred_y)
+                pre_list_prob.append(pred_prob)
+
+                #计算精确率准确率召回率和F值
+                acc, p, r, f1 = func.acc_prf(pred_y, true_y, te_doc_len)
+                precision_list.append(p)
+                recall_list.append(r)
+                FF1_list.append(f1)
+                if f1 > max_f1:
+                    max_acc, max_p, max_r, max_f1 = acc, p, r, f1
+                print('\ntest: epoch {}: loss {:.4f} acc {:.4f}\np: {:.4f} r: {:.4f} f1: {:.4f} max_f1 {:.4f}\n'.format(
+                    epoch + 1, loss, acc, p, r, f1, max_f1))
+
+            Id.append(len(te_x))
+            SID = np.sum(Id) - len(te_x)
+            _, maxIndex = func.maxS(FF1_list)
+            print("maxIndex:", maxIndex)
+            print('Optimization Finished!\n')
+            pred_prob = pre_list_prob[maxIndex]
+
+            for i in range(pred_y.shape[0]):
+                for j in range(te_doc_len[i]):
+                    prob_list_pr.append(pred_prob[i][j][1])
+                    y_label.append(true_y[i][j])
+
+            p_list.append(max_p)
+            r_list.append(max_r)
+            f1_list.append(max_f1)
+        print("running time: ", str((end_time - start_time) / 60.))
+        print_training_info()
+        p, r, f1 = map(lambda x: np.array(x).mean(), [p_list, r_list, f1_list])
+        print("f1_score in 10 fold: {}\naverage : {} {} {}\n".format(np.array(f1_list).reshape(-1, 1), round(p, 4), round(r, 4), round(f1, 4)))
 
         return p, r, f1
-
-#调用该函数前需要tf.reset_default_graph()
-def reload():
-    saver = tf.train.import_meta_graph("run/model.ckpt.meta")
-    with tf.Session() as sess:
-        saver.restore(sess, "./run/model.ckpt")
 
 def print_training_info():
     print('\n\n>>>>>>>>>>>>>>>>>>>>TRAINING INFO:\n')
