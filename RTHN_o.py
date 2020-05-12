050e44de4d78142eb003222d16faa2dda490c561
1037562613@qq.com <1037562613@qq.com>
2020-05-12 14:55:11 +0800

    1111111

--- RTHN_o.py
+++ RTHN_o.py
@@ -8,7 +8,7 @@ import sys, os, time, codecs, pdb
 import utils.tf_funcs as func
 from sklearn.model_selection import KFold
 from sklearn.model_selection import ParameterGrid
-os.environ["CUDA_VISIBLE_DEVICES"] = '5, 0'
+os.environ["CUDA_VISIBLE_DEVICES"] = '0,1'
 
 FLAGS = tf.app.flags.FLAGS
 # >>>>>>>>>>>>>>>>>>>> For Model <<<<<<<<<<<<<<<<<<<< #
@@ -343,7 +343,7 @@ def trans_func(senEncode_dis, senEncode, n_feature, out_units, scope_var):
 def main(_):
     grid_search = {}
     # params = {"n_layers": [4, 5]}
-    params = {"n_layers": [4]}
+    params = {"n_layers": [3,4,5]}
 
     params_search = list(ParameterGrid(params))
 
