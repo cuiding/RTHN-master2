d76716a2d1d90a00e5023f350dab1c69512ba039
1037562613@qq.com <1037562613@qq.com>
2020-06-06 06:57:20 +0800

    1111111

--- RTHN_o.py
+++ RTHN_o.py
@@ -8,7 +8,7 @@ import sys, os, time, codecs, pdb
 import utils.tf_funcs as func
 from sklearn.model_selection import KFold
 from sklearn.model_selection import ParameterGrid
-os.environ["CUDA_VISIBLE_DEVICES"] = '0,5,6'
+os.environ["CUDA_VISIBLE_DEVICES"] = '0,5,6,4'
 
 FLAGS = tf.app.flags.FLAGS
 # >>>>>>>>>>>>>>>>>>>> For Model <<<<<<<<<<<<<<<<<<<< #
